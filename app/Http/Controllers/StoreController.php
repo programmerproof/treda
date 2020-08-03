87f08f0027e9af9cfc641809a35538105d090184
unknown <segquerenquer@gmail.com>
2020-08-02 22:26:50 -0500

    Creación: CRUD Controller Tienda

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
+use App\User;
+use App\Store;
+use App\Product;
 
 class StoreController extends Controller
 {
@@ -11,9 +15,17 @@ class StoreController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+        $user_id = \Auth::id();
+        $search = $request->get('search');
+        if($search != ''){
+            $stores = Store::nameLike($search)->paginate();
+        }else{
+            $stores = Store::with('products')->OrderByNameAsc()->paginate();
+        }
+        return view('store.index', compact('stores'))->with('user', $user_id); 
     }
 
     /**
@@ -24,6 +36,7 @@ class StoreController extends Controller
     public function create()
     {
         //
+        return view('store.create');
     }
 
     /**
@@ -35,6 +48,42 @@ class StoreController extends Controller
     public function store(Request $request)
     {
         //
+        $exception = '';
+        try{
+            $user_id = \Auth::id();
+            $user = User::find( $user_id);
+            $store = new Store();
+            $store->user()->associate($user);
+            $this->validate($request,
+                            [
+                            'name'=>'required|regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:30', 
+                            'opening_date'=>'required|regex:/^([0-9]{1,2})\\-([0-9]{1,2})\\-([0-9]{4})$/', 
+                            ]
+                        ); 
+            $store->name = $request->name;
+            $opening_date = \DateTime::createFromFormat('d-m-Y', $request->opening_date)->format('Y-m-d');
+            $store->opening_date = $opening_date;
+            $store->created_at = date('Y-m-d H:i:s', time());
+            $store->save();
+        }catch(\Illuminate\Database\QueryException $e){
+                $errorCode = $e->errorInfo[1];
+                $err = '';
+                switch ($errorCode) {
+                    case 1364:
+                        $err = response([
+                            'errors'=>$e->getMessage()
+                        ],Response::HTTP_NOT_FOUND);
+                        $exception = 'Error de trasabilidad.';
+                    break;
+                }
+        }
+            
+        if($exception != ''){
+                return redirect()->back()->with('errorAccess', $exception)->withInput($request->all());
+        }else {
+                return redirect()->route('store.index')->with('success','Registro creado satisfactoriamente.')
+                                ->with('action','create');     
+            }  
     }
 
     /**
@@ -46,6 +95,20 @@ class StoreController extends Controller
     public function show($id)
     {
         //
+        $show = Store::validate(
+        (object) [
+                'name' => 'id',
+                'value' => $id,
+                'success' => function() use($id){
+                    $stores = Store::where('id', $id)->with('products')->get();
+                    return view('store.show', compact('stores'));
+                },
+                'err' => function(){
+                    return redirect()->route('store.index')->with('errorAccess', 'Esa Tienda no existe.');
+                }  
+            ]
+        );
+        return $show;
     }
 
     /**
@@ -57,28 +120,25 @@ class StoreController extends Controller
     public function edit($id)
     {
         //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $edit = Store::validate(
+        (object) [
+                'name' => 'id',
+                'value' => $id,
+                'success' => function() use($id){
+                    $user_id = \Auth::id();
+                    $stores = Store::where('id', $id)->with('products')->get();
+                    
+                    if($stores[0]->user_id != $user_id){
+                        return redirect()->route('store.index')->with('errorAccess', 'No tiene acceso a esa Tienda.');
+                    }else{
+                        return view('store.edit', compact('stores'));
+                    }
+                },
+                'err' => function(){
+                    return redirect()->route('store.index')->with('errorAccess', 'Esa Sucursal no existe.');
+                }  
+            ]
+        );
+        return $edit;
     }
 }
