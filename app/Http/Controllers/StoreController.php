611e76148cb2cee3414a7361dc264af971951fa0
unknown <segquerenquer@gmail.com>
2020-08-02 22:27:46 -0500

    Creación: Service REST Controller Tienda

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -141,4 +141,141 @@ class StoreController extends Controller
         );
         return $edit;
     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $exception = '';
+    try{
+        $user_id = \Auth::id();
+        $user = User::find($user_id);
+        $store = Store::find($id);
+        $store->user()->associate($user);
+        $this->validate($request,
+                        [
+                         'name'=>'required|regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:30', 
+                         'opening_date'=>'required|regex:/^([0-9]{1,2})\\-([0-9]{1,2})\\-([0-9]{4})$/', 
+                        ]
+                       );
+        $store->name = $request->name;
+        $opening_date = \DateTime::createFromFormat('d-m-Y', $request->opening_date)->format('Y-m-d');
+        $store->opening_date = $opening_date;
+        $store->updated_at = date('Y-m-d H:i:s', time());
+        $store->save();
+    }catch(\Illuminate\Database\QueryException $e){
+        $errorCode = $e->errorInfo[1];
+        $err = '';
+        switch ($errorCode) {
+            case 1364:
+                $err = response([
+                    'errors'=>$e->getMessage()
+                ],Response::HTTP_NOT_FOUND);
+                $exception = 'Error de trasabilidad consulte con el Dpto. de Sistemas.';
+            break;
+        }
+    }
+    
+    if($exception != ''){
+        return redirect()->back()->with('errorAccess', $exception)->withInput($request->all());
+    }else {
+        return redirect()->route('store.index')->with('success','Registro actualizado satisfactoriamente.')
+                         ->with('action','edit');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $destroy = Store::validate(
+        (object) [
+                'name' => 'id',
+                'value' => $id,
+                'success' => function() use($id){
+                    $user_id = \Auth::id();
+                    $stores = Store::find($id);
+
+                    if($stores->user_id != $user_id){
+                        return redirect()->route('store.index')->with('errorAccess', 'No tiene acceso a esa Tienda.');
+                    }else{
+                        $stores->delete();
+                        return redirect()->route('store.index')->with('success','Registro eliminado satisfactoriamente.')
+                             ->with('action','destroy');
+                    }
+                },
+                'err' => function(){
+                    return redirect()->route('store.index')->with('errorAccess', 'Esa Tienda no existe.');
+                }  
+            ]
+        );
+        return $destroy;
+    }
+
+    /**
+     * Display the specific resource
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search(Request $request, $id)
+    {
+        //
+        $result = Store::scopeWhereId($id);
+        return \Response::json($result);
+    }
+
+    /**
+     * Display the specific resource
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function info(Request $request)
+    {
+        //
+        $store_id = $request->get('id');
+        $response = Product::info(
+            (object) [
+                       'store_id' =>  $store_id,
+                       'file' => 'public/infoProduct.log',
+                       'request' => $request,
+                       'name' => 'image',
+                       'data' => Product::where('store_id', $store_id)->get([
+                                                                     'store_id',
+                                                                     'sku',
+                                                                     'name', 
+                                                                     'description',
+                                                                     'value',
+                                                                     'image'
+                                                                    ]),
+                      'success' => function($data) {
+                        return array(['result' => array([
+                                                    'success' => true,
+                                                    'message' => 'The data was obtained satisfactorily.',
+                                                    'data' => $data
+                                                ])]);    
+                        }, 
+                        'err' => function(){
+                            return array(['result' => array([
+                                                       'success' => false,
+                                                       'message' => 'There were no data.',
+                                                       'data' => array()
+                                                     ])]);
+                        }  
+                    ]
+                ); 
+        return \Response::json($response);
+    }
 }
