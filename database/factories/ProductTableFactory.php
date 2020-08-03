ead5c44e68dd1c350a4eeb7fabde2d494e07ad5c
unknown <segquerenquer@gmail.com>
2020-08-02 21:08:30 -0500

    Parametrización: Factory de las tablas tienda y producto

--- database/factories/ProductTableFactory.php
+++ database/factories/ProductTableFactory.php
@@ -2,8 +2,14 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Product::class, function (Faker $faker) {
     return [
         //
+        'sku' => $faker->unique()->swiftBicNumber(),
+        'name' => $faker->unique()->firstNameMale(),
+        'description' => $faker->unique()->realText($maxNbChars = 50, $indexSize = 2),
+        'value' => $faker->unique()->ean8(),
+        'image' => $faker->imageUrl($width = 60, $height = 48, 'cats'),
+        'created_at' => date("Y-m-d H:i:s", time())
     ];
 });
--- database/factories/StoreTableFactory.php
+++ database/factories/StoreTableFactory.php
@@ -2,8 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Store::class, function (Faker $faker) {
     return [
         //
+        'name' => $faker->unique()->company(),
+        'opening_date' => $faker->unique()->date($format = 'Y-m-d', $max = 'now'),
+        'created_at' => date("Y-m-d H:i:s", time())
     ];
 });
