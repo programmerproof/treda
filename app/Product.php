834162ca60f12b05fd4032a435606f786e419cc9
unknown <segquerenquer@gmail.com>
2020-08-02 21:35:56 -0500

    Parametrización: Model de las tablas tienda y producto (Agregamos los atributos necesarios para su funcionamiento básco, al igual que aplicamos SoftDeletes para la "eliminación"(ocultación) de los registros).

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $table = 'product';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $guarded = ['_token'];
 }
--- app/Store.php
+++ app/Store.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Store extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $table = 'store';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['name'];
+
+    public $timestamps = false;
+
+    protected $guarded = ['_token'];
 }
