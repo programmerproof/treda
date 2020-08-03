e89830643ae75fdf636fffff66f5f964dcbd31b7
unknown <segquerenquer@gmail.com>
2020-08-02 21:13:13 -0500

    Parametrización: Seeder de las tablas tienda=>(150) y producto=>(5) registros

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Store;
+use App\Product;
 
 class ProductTableSeeder extends Seeder
 {
@@ -12,5 +15,13 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         //
+        $user_id = User::orderBy('id', 'ASC')->first()->id;
+        $store_id = Store::orderBy('id', 'ASC')->first()->id;
+        factory(Product::class, 5)->create(['user_id' => $user_id, 'store_id' => $store_id]);
+    }
+
+    public function down()
+    {
+        \DB::table('product')->delete();
     }
 }
--- database/seeds/StoreTableSeeder.php
+++ database/seeds/StoreTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Store;
 
 class StoreTableSeeder extends Seeder
 {
@@ -12,5 +14,12 @@ class StoreTableSeeder extends Seeder
     public function run()
     {
         //
+        $user_id = User::orderBy('id', 'ASC')->first()->id;
+        factory(Store::class, 150)->create(['user_id' => $user_id]);
+    }
+
+    public function down()
+    {
+        \DB::table('store')->delete();
     }
 }
