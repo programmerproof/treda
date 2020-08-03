9783dc7349ef6c39d16f74e541f9c2ebff997fad
unknown <segquerenquer@gmail.com>
2020-08-02 22:29:59 -0500

    Creación: CRUD Controller Producto (Validacion campo valor numerico, fecha formato dd-mm-YY a través de expreciones regulares)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
+use App\User;
+use App\Store;
+use App\Product;
 
 class ProductController extends Controller
 {
@@ -11,9 +15,17 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+        $user_id = \Auth::id();
+        $search = $request->get('search');
+        if($search != ''){
+            $products = Product::skuLike($search)->paginate();
+        }else{
+            $products = Product::with('store')->OrderByNameAsc()->paginate();
+        }
+        return view('product.index', compact('products'))->with('user', $user_id); 
     }
 
     /**
@@ -24,6 +36,8 @@ class ProductController extends Controller
     public function create()
     {
         //
+        $stores = Store::orderByNameAsc()->get();
+        return view('product.create', compact('stores'));
     }
 
     /**
@@ -35,6 +49,70 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
+        $exception = '';
+        try{
+            $user_id = \Auth::id();
+            $user = User::find($user_id);
+            $store = Store::find($request->store_id);
+            $product = new Product();
+            $product->user()->associate($user);
+            $product->store()->associate($store);
+            $this->validate($request,
+                            [
+                            'sku'=>'required|regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:30', 
+                            'name'=>'required|regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:30', 
+                            'description'=>'required|regex:/^([0-9,.()a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9,.()a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:300', 
+                            'value'=>'required|numeric|min:1|max:999999999', 
+                            'store_id'=>'required|numeric',
+                            'image' => 'required|mimes:jpeg,jpg,png,gif,svg|max:10000',
+                            ]
+                        );            
+            $product->sku = $request->sku;
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->value = $request->value;
+            $exception = Product::file(
+                (object) [
+                          'type' => 'create',
+                          'name' => 'image',
+                          'data' => $request,
+                          'prev' => '', 
+                          'path' => 'img/product/',  
+                          'upload' => function($data) use($product){
+                                $product->image = $data;
+                            }, 
+                            'err' => function(){
+                                return 'Error cargando el archivo.';
+                            }  
+                        ]
+                    );      
+            $product->created_at = date('Y-m-d H:i:s', time());
+            $product->save();
+        }catch(\Illuminate\Database\QueryException $e){
+                $errorCode = $e->errorInfo[1];
+                $err = '';
+                switch ($errorCode) {
+                    case 1062: 
+                        $err = response([
+                            'errors'=>$e->getMessage()
+                        ],Response::HTTP_NOT_FOUND);
+                        $exception = 'El sku '.$request->sku.' ya existe.';
+                    break;
+                    case 1364:
+                        $err = response([
+                            'errors'=>$e->getMessage()
+                        ],Response::HTTP_NOT_FOUND);
+                        $exception = 'Error de trasabilidad.';
+                    break;
+                }
+        }
+            
+        if($exception != ''){
+                return redirect()->back()->with('errorAccess', $exception)->withInput($request->all());
+        }else {
+                return redirect()->route('product.index')->with('success','Registro creado satisfactoriamente.')
+                                ->with('action','create');     
+            }
     }
 
     /**
@@ -43,9 +121,23 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($sku)
     {
         //
+        $show = Product::validate(
+            (object) [
+                'name' => 'sku',
+                'value' => $sku,
+                'success' => function() use($sku){
+                    $products = Product::where('sku', $sku)->with('store')->get();
+                    return view('product.show', compact('products'));
+                },
+                'err' => function(){
+                    return redirect()->route('product.index')->with('errorAccess', 'Ese Producto no existe.');
+                }  
+            ]
+        );
+        return $show;
     }
 
     /**
@@ -57,6 +149,27 @@ class ProductController extends Controller
     public function edit($id)
     {
         //
+        $edit = Product::validate(
+        (object) [
+                  'name' => 'id',
+                  'value' => $id,
+                  'success' => function() use($id){
+                        $user_id = \Auth::id();
+                        $stores = Store::orderByNameAsc()->get();
+                        $products = Product::where('id', $id)->with('store')->get();
+                        
+                        if($products[0]->user_id != $user_id){
+                            return redirect()->route('product.index')->with('errorAccess', 'No tiene acceso a ese Producto.');
+                        }else{
+                            return view('product.edit', compact('products', 'stores'));
+                        }
+                    },
+                    'err' => function(){
+                        return redirect()->route('product.index')->with('errorAccess', 'Ese Producto no existe.');
+                    }  
+                ]
+            );
+            return $edit;
     }
 
     /**
@@ -69,6 +182,64 @@ class ProductController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $exception = '';
+    try{
+        $user_id = \Auth::id();
+        $user = User::find($user_id);
+        $store = Store::find($request->store_id);
+        $product = Product::find($id);
+        $product->user()->associate($user);
+        $product->store()->associate($store);
+        $this->validate($request,
+                        [
+                         'sku'=>'required|regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:30', 
+                         'name'=>'required|regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:30', 
+                         'description'=>'required|regex:/^([0-9,.()a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9,.()a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/|max:300', 
+                         'value'=>'required|numeric|min:1|max:999999999', 
+                         'store_id'=>'required|numeric',
+                         'image' => 'mimes:jpeg,png,jpg,gif,svg|max:2048', 
+                        ]
+                       );
+        $product->sku = $request->sku;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->value = $request->value;   
+        $exception = Product::file(
+            (object) [
+                      'type' => 'update',
+                      'name' => 'image',
+                      'data' => $request,
+                      'prev' => $product->image, 
+                      'path' => 'img/product/',  
+                      'upload' => function($data) use($product){
+                            $product->image = $data;
+                        }, 
+                        'err' => function(){
+                            return 'Error cargando el archivo.';
+                        }  
+                    ]
+                );
+        $product->updated_at = date('Y-m-d H:i:s', time());
+        $product->save();
+    }catch(\Illuminate\Database\QueryException $e){
+        $errorCode = $e->errorInfo[1];
+        $err = '';
+        switch ($errorCode) {
+            case 1364:
+                $err = response([
+                    'errors'=>$e->getMessage()
+                ],Response::HTTP_NOT_FOUND);
+                $exception = 'Error de trasabilidad.';
+            break;
+        }
+    }
+    
+    if($exception != ''){
+        return redirect()->back()->with('errorAccess', $exception)->withInput($request->all());
+    }else {
+        return redirect()->route('product.index')->with('success','Registro actualizado satisfactoriamente.')
+                         ->with('action','edit');
+        }
     }
 
     /**
@@ -80,5 +251,27 @@ class ProductController extends Controller
     public function destroy($id)
     {
         //
+        $destroy = Product::validate(
+            (object) [
+                'name' => 'id',
+                'value' => $id,
+                'success' => function() use($id){
+                    $user_id = \Auth::id();
+                    $products = Product::find($id);
+
+                    if($products->user_id != $user_id){
+                        return redirect()->route('product.index')->with('errorAccess', 'No tiene acceso a ese Producto.');
+                    }else{
+                        $products->delete();
+                        return redirect()->route('product.index')->with('success','Registro eliminado satisfactoriamente.')
+                             ->with('action','destroy');
+                    }
+                },
+                'err' => function(){
+                    return redirect()->route('product.index')->with('errorAccess', 'Ese Empleado no existe.');
+                }  
+            ]
+        );
+        return $destroy;
     }
 }
