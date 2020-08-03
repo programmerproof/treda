7a256d7a58bef2b9809fa23456f649766c2b4ee3
unknown <segquerenquer@gmail.com>
2020-08-02 21:15:54 -0500

    Modificación: DataBaseSeeder agregando los Seeder de las tablas tienda y producto para ejecutar los de prueba conjuntamente

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(StoreTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
     }
 }
