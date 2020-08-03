36c487d28c85943387d72781a1673ca06e02df9d
unknown <segquerenquer@gmail.com>
2020-08-02 22:31:16 -0500

    Creación: de la visualización del log generado a traves del servicio REST

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -14,71 +14,23 @@ class LogController extends Controller
     public function index()
     {
         //
+        $content = array();
+        $name = 'public/infoProduct.log';
+        $exist = \Storage::exists($name); 
+        if($exist){
+           $content = explode("\r\n", \Storage::get($name));
+        }
+        return view('log.index', compact('content'));
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function message()
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $data = '';
+        return view('home')->with('message', $data);
     }
 }
